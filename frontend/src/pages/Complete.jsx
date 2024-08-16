2943c2cdbfe159e81f3043d4bb4312ab2fada780
smallsilverwest <smallsilverwest@ewha.ac.kr>
2024-08-16 15:55:07 +0900

    feat(complete): 회원가입 완료 페이지 구현

--- frontend/src/pages/Complete.jsx
+++ frontend/src/pages/Complete.jsx
@@ -1,10 +1,18 @@
-import '../styles/complete.css';
+import { useNavigate } from 'react-router-dom';
+import '../styles/start/complete.css';
 
 function Complete() {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate('/');
+  };
+
   return (
     <div className="complete">
-      <h1>회원가입 완료</h1>
-      <p>회원가입이 성공적으로 완료되었습니다!</p>
+      <img src="/images/check.png" alt="체크" className="check" />
+      <h1>회원가입이 완료되었습니다.</h1>
+      <button className="btn3" type="button" onClick={handleButtonClick}> 로그인 화면으로 돌아가기 </button>
     </div>
   );
 }
