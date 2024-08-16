a8d7f043cd0c1a56c44260b1e6edeae84e95ccce
smallsilverwest <smallsilverwest@ewha.ac.kr>
2024-08-16 15:58:02 +0900

    chore: 페이지별 일부 코드 수정

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,18 +1,25 @@
-import '../styles/login.css';
-import mainicon from '../MainpageIcon.png';
+import { Link, useNavigate } from 'react-router-dom';
+import "../styles/start/login.css";
 
 function Login() {
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate('/MainPage');
+  };
+
   return (
     <div className="login">
-      <img src={mainicon} alt="시작화면" className="icon" />
+      <img src="/images/MainpageIcon.png" alt="시작화면" className="icon" />
       <h1>병원 키오스크 연습 웹</h1>
       <h2>홀로서기</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="text_area">
           <input
             type="text"
-            id="username"
-            name="username"
+            id="id"
+            name="id"
             placeholder="아이디"
             className="text_input"
           />
@@ -20,19 +27,19 @@ function Login() {
         <div className="text_area">
           <input
             type="password"
-            id="password"
-            name="password"
+            id="pw"
+            name="pw"
             placeholder="비밀번호"
             className="text_input"
           />
         </div>
-        <input type="submit" value="로그인" className="btn" />
+
+        <button className="btn" type="submit">로그인</button>
       </form>
-      <a className="link" href="/signup">
-        처음 오셨나요?
-      </a>
+      <Link className="link" to="/signup">처음 오셨나요?</Link>
+      <Link className="link" to="/MainPage">로그인 없이 둘러보기</Link>
     </div>
-  );
+  )
 }
 
 export default Login;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react';
-import '../styles/signup.css';
+import { useNavigate, Link } from 'react-router-dom';
+import "../styles/start/signup.css";
 
 function Signup() {
   const [formData, setFormData] = useState({
-    username: '',
+    name: '',
     id: '',
-    password: '',
+    pw: '',
     phone: '',
     identify: '',
   });
+
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -16,66 +19,68 @@ function Signup() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Signup form submitted');
+    console.log('Signup form submitted', formData);
+    navigate('/complete');
   };
 
   return (
-    <div className="login">
+    <div className="signup">
       <h5>홀로서기</h5>
       <h6>병원 키오스크 연습 웹</h6>
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <h7>이름</h7>
         <input
           className="text_area"
           type="text"
-          name="username"
+          name="name"
           placeholder="이름을 입력해주세요."
-          value={this.state.username}
-          onChange={this.handleChange}
+          value={formData.name}
+          onChange={handleChange}
         />
-        <h8>아이디</h8>
+
+        <h7>아이디</h7>
         <input
           className="text_area"
           type="text"
           name="id"
           placeholder="아이디를 입력해주세요."
-          value={this.state.id}
-          onChange={this.handleChange}
+          value={formData.id}
+          onChange={handleChange}
         />
-        <h9>비밀번호</h9>
+
+        <h7>비밀번호</h7>
         <input
           className="text_area"
           type="password"
-          name="password"
+          name="pw"
           placeholder="비밀번호"
-          value={this.state.password}
-          onChange={this.handleChange}
+          value={formData.pw}
+          onChange={handleChange}
         />
-        <h10>전화번호</h10>
+
+        <h7>전화번호</h7>
         <input
           className="text_area"
           type="text"
           name="phone"
           placeholder="전화번호(-제외)"
-          value={this.state.phone}
-          onChange={this.handleChange}
+          value={formData.phone}
+          onChange={handleChange}
         />
-        <h11>주민번호</h11>
+
+        <h7>주민번호</h7>
         <input
           className="text_area"
           type="text"
-          name="identify"
+          name="ssn"
           placeholder="주민번호(-제외)"
-          value={this.state.identify}
-          onChange={this.handleChange}
+          value={formData.ssn}
+          onChange={handleChange}
         />
-        <button className="btn" type="submit">
-          회원가입
-        </button>
+
+        <button className="btn2" type="submit">회원가입</button>
       </form>
-      <a className="goback" href="/login">
-        계정이 이미 있으신가요?
-      </a>
+      <Link className="goback" to="/">계정이 이미 있으신가요?</Link>
     </div>
   );
 }
